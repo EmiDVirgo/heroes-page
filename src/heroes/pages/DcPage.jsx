cd68bf077dd314b737edd7d4a65aa9421c277548
Emile <emilejoel_05@hotmail.com>
2024-01-11 20:49:45 -0500

    first commit

---
+++ src/heroes/pages/DcPage.jsx
@@ -0,0 +1,16 @@
+import { HeroList } from "../components/HeroList"
+
+
+export const DcPage = () => {
+
+  
+  return (
+    <>
+      <h1>DC Page</h1>
+
+      <HeroList publisher={'DC Comics'}/>
+      
+      
+    </>
+  )
+}
